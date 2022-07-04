cb7601690fd4ae18482c7dce664268f607a6dc4b
eunhyung <ooooo5h91@gmail.com>
2022-07-05 06:00:46 +0900

    Add: Movies app created and made models.py

---
+++ movies/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Actor(models.Model):
+	first_name = models.CharField(max_length=45)
+	last_name = models.CharField(max_length=45)
+	date_of_birth = models.DateField()
+	
+	class Meta:
+		db_table = 'actors'
+
+class Movie(models.Model):
+	title = models.CharField(max_length=45)
+	release_date = models.DateField()
+	running_time = models.IntegerField()
+
+	class Meta:
+		db_table = 'movies'
+
+class ActorAndMoive(models.Model):
+	actor = models.ForeignKey(Actor, on_delete = models.CASCADE)
+	movie = models.ForeignKey(Movie, on_delete = models.CASCADE)
+
+	class Meta:
+		db_table = 'actors_movies'
